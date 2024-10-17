0da2a21d7fc0680e4e8d3ccfb7d315e251938c70
renzJaskin <jaskin.agmata@gmail.com>
2024-10-17 16:39:51 +0800

    feat: added router

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var connectDb = require('./connect-db.js');
 
+
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/authRoutes');
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
